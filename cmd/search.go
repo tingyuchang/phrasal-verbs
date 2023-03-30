8aaa23de012660db25bbba85b9c86c476883d35a
tingyuchang <tingyugenius@gmail.com>
2023-03-30 14:06:11 +0800

    ADD: search

--- cmd/search.go
+++ cmd/search.go
@@ -6,7 +6,6 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -21,20 +20,30 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("search called")
+		key, _ := cmd.Flags().GetString("key")
+
+		if key == "" {
+			fmt.Println("Please enter valid key.")
+			return
+		}
+
+		//TODO add regex search
+		//TODO fulltext search
+
+		_, ok := index[key]
+
+		if ok {
+			verb := data[index[key]]
+			text, _ := PrettyPrintJSONstream(verb)
+			fmt.Println(text)
+			return
+		}
+
+		fmt.Printf("%s not found\n", key)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(searchCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// searchCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// searchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	searchCmd.Flags().StringP("key", "k", "", "search key for query verb's name")
 }
